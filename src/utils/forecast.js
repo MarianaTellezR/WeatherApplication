a90f8ba8db2919090478a8a9a8e795419f381851
Mariana <tellezresendizmariana@gmail.com>
2022-06-03 13:49:19 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=ec7b3837bda286850deccb89de2fb33b&query=' + latitude + ',' + longitude + '&units=m'
+    request({ url, json: true }, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' °C degrees out. It feels like ' + body.current.feelslike + ' °C degrees out.')
+        }
+
+    })
+
+}
+
+module.exports = forecast
